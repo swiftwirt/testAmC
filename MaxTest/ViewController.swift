fc48c2dfe9511b18676bce366385a6e52661a4ae
Dmitry Ivashin <dmitry@lunapps.com>
2018-01-26 15:00:45 +0200

    Initial Commit

---
+++ MaxTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaxTest
+//
+//  Created by Dmitry Ivashin on 1/26/18.
+//  Copyright © 2018 Dmitry Ivashin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
